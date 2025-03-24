9f2eb77158e53c32534284aa94c0e524fd8121e5
devreissc <reis.rrsc@outlook.com>
2025-03-24 10:47:42 -0300

    fix: Correção tabela de tarefas

--- resources/views/app/tasks/index.blade.php
+++ resources/views/app/tasks/index.blade.php
@@ -36,6 +36,7 @@
                                     <td>
                                         
                                     </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
