0b305d4597d94b9055a819931873ae6694391e9f
devreissc <reis.rrsc@outlook.com>
2025-03-25 10:17:40 -0300

    db: Ajuste e criação de migrations

---
+++ database/migrations/2025_03_20_212418_alter_table_subtasks_add_relationship_tasks.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subtasks', function (Blueprint $table) {
+            $table->unsignedBigInteger('task_id')->nullable()->after('id');
+            $table->foreign('task_id')->references('id')->on('tasks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subtasks', function (Blueprint $table) {
+            $table->dropForeign(['task_id']);
+            $table->dropColumn('task_id');
+        });
+    }
+};
