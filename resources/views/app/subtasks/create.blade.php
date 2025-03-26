eaf7591ac992c01108a0775f0dacf5d4a4f06709
devreissc <reis.rrsc@outlook.com>
2025-03-26 18:51:20 -0300

    refactor: Views da otimização create/edit subtask

--- resources/views/app/subtasks/create.blade.php
+++ resources/views/app/subtasks/create.blade.php
@@ -2,45 +2,9 @@
     <div class="d-flex align-items-center justify-content-center vh-100 bg-light">
         <div class="p-4 bg-white shadow rounded" style="width: 100%; max-width: 400px;">
             <h3 class="text-center mb-3">Nova subtarefa</h3>
+            
+            <x-form-create-edit-subtask :users="$users" :tasks="$tasks" :action="route('subtarefas.store')" />
 
-            <form method="POST" action="{{ route('subtarefas.store') }}">
-                @csrf
-                <div class="mb-3">
-                    <label for="task_id" class="form-label">Tarefa</label>
-                    <select class="form-select" id="task_id" name="task_id">
-                        <option selected>Selecione uma tarefa</option>
-                        @foreach($tasks as $task)
-                            <option value="{{ $task->id }}">{{ $task->task_name }}</option>
-                        @endforeach
-                    </select>
-                </div>
-                <div class="mb-3">
-                    <label for="subtask_name" class="form-label">Subtarefa</label>
-                    <input type="text" class="form-control" id="subtask_name" name="subtask_name">
-                    {{ $errors->has('subtask_name') ? $errors->first('subtask_name') : '' }}
-                </div>
-                <div class="mb-3">
-                    <label for="subtask_description" class="form-label">Descrição</label>
-                    <textarea class="form-control" id="subtask_description" name="subtask_description" rows="3"></textarea>
-                    {{ $errors->has('subtask_description') ? $errors->first('subtask_description') : '' }}
-                </div>
-                <div class="mb-3">
-                    <label for="due_date" class="form-label">Data de conclusão</label>
-                    <input type="date" class="form-control" id="due_date" name="due_date">
-                    {{ $errors->has('due_date') ? $errors->first('due_date') : '' }}
-                </div>
-                <div class="mb-3">
-                    <label for="due_date" class="form-label">Horário de conclusão</label>
-                    <input type="time" class="form-control" id="due_time" name="due_time">
-                    {{ $errors->has('due_time') ? $errors->first('due_time') : '' }}
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" name="is_complete" class="form-check-input" id="is_complete">
-                    <label class="form-check-label" for="is_complete">Finalizada</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
         </div>
     </div>
-    
 </x-app-layout>
