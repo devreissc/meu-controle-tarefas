9a326e953c88847a6a5499c42984e6f3d7fd1e5a
devreissc <reis.rrsc@outlook.com>
2025-03-27 09:03:55 -0300

    feat: Relacionamento Subtask com User

--- app/Models/Subtask.php
+++ app/Models/Subtask.php
@@ -16,4 +16,9 @@ class Subtask extends Model
     {
         return $this->belongsTo(Task::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
